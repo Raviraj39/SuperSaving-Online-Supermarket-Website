a430a7d434bc9bfeab2d4ae42c0fea330fb561ea
Raviraj39 <rravirajbujawade@gmail.com>
2023-12-26 23:09:56 +0530

    Added files

---
+++ Super_market/src/main/java/com/org/Register2.java
@@ -0,0 +1,43 @@
+package com.org;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Register2")
+public class Register2 extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String username = request.getParameter("input1");
+		String password = request.getParameter("input2");
+		String repassword = request.getParameter("input3");
+		String email = request.getParameter("input4");
+		String mob = request.getParameter("input5");
+
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/super_marketone","root","root");
+			Statement stat = conn.createStatement();
+			stat.executeUpdate("insert into register (username,password,email,mob_num) values('"+username+"','"+password+"','"+email+"','"+mob+"')");
+			//out.println("data inserted");
+			response.sendRedirect("Login.jsp");
+			
+			conn.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
